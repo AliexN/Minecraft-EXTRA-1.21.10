5322a337b6fffaa3bf24c40c1de5bfd0df330702
AliexN <aliexnunity@gmail.com>
2025-11-11 20:26:47 +0300

    SILVER ORE IS READY!!!!!

--- src/main/java/net/aquamar1n/mineextra/registry/ModWorldGen.java
+++ src/main/java/net/aquamar1n/mineextra/registry/ModWorldGen.java
@@ -1,10 +1,6 @@
 package net.aquamar1n.mineextra.registry;
 
-import net.aquamar1n.mineextra.AquaMod;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.neoforged.neoforge.registries.datamaps.RegisterDataMapTypesEvent;
